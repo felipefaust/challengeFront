8d1af1a907a2727e56ba27483bf295a8655f467c
felipefaust <faustfelipe@gmail.com>
2017-07-10 21:47:47 -0300

    corrigindo api

--- public/js/controllers/main-controller.js
+++ public/js/controllers/main-controller.js
@@ -1,21 +1,35 @@
 angular.module('desafiofront').controller('mainController',['$scope','cities', 'weatherApi',function($scope, cities, weatherApi){
-  $scope.go = function(){
-    alert($scope.testebotao);
-  }
   $scope.states = cities.estados;
 
-
-   $scope.showWeather = function(){
+  $scope.showWeather = function(){
 
      if(!$scope.state || !$scope.cityName) {
              $scope.messageErr = 'missingState';
              return;
       }
 
-      weatherApi.consult()
-         .then(function(result) {
-           console.log(result);
-         })
+    weatherApi.consult($scope.cityName, $scope.state.sigla)
+    .then(function(result) {
+      var arr = [];
+      result.data.list.forEach(function(weather){
+        var dt = new Date(weather.dt * 1000);
+        arr.push({
+          data: dt,
+          descricao: weather.weather[0].description,
+          imagem: weather.weather[0].icon,
+          temperatura_min: weather.temp.min,
+          temperatura_max: weather.temp.max
+        });
+      });
+      result.data.previsoes = arr;
+      $scope.weatherInfo = result.data;
+      console.log($scope.weatherInfo.previsoes);
+    },
+    function(err) {
+        $scope.messageErr = "Não foi possível obter as informações de tempo para a cidade desejada."
+    });
+
+
 
    }
 }]);
--- public/js/services/weatherApi.js
+++ public/js/services/weatherApi.js
@@ -2,6 +2,7 @@
 app.service('weatherApi', ['$http', function($http){
 
   this.consult = function(city, state) {
-    return  $http.get('http://api.openweathermap.org/data/2.5/weather?q=blumenau,br&mode=json&units=metric&APPID=78192eebc1863801146921a23b842ed2', {cache: true});
+    return $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=blumenau,br&cnt=5&units=metric&APPID=78192eebc1863801146921a23b842ed2');
+    //return  $http.get('http://api.openweathermap.org/data/2.5/daily?q='+city.toLowerCase()+',br&cnt=5&lang=pt&mode=json&units=metric&APPID=78192eebc1863801146921a23b842ed2', {cache: true});
   }
 }]);
