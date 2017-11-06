b57e0dfdeb1bfd6271f470191e362c4fb32344b2
Felipe Faust <felipe.faust@senior.com.br>
2017-11-06 09:42:23 -0200

    alteracao layout

--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -1,7 +1,7 @@
 angular.module('meusServicos', ['ngResource'])
     .factory('recursoFoto', function($resource) {
 
-        return $resource('api.openweathermap.org/data/2.5/forecast/daily?id=:idCidade&cnt=5', null, {
+        return $resource('api.openweathermap.org/data/2.5/forecast/daily?id=:idCidade&cnt=10', null, {
             'update' : { 
                 method: 'PUT'
             }
